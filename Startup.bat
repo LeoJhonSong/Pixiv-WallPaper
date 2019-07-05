d1b30b90664345c89e78c6c69ad83cb202d7cffe
Leo-Dell-Win <LeoJhon.Song@outlook.com>
2019-07-05 21:29:00 +0800

    improved the logic and added more comments , added bacth file to startup

---
+++ Startup.bat
@@ -0,0 +1,3 @@
+@echo off
+start pythonw .\wallpaper.py
+exit
--- wallpaper.py
+++ wallpaper.py
@@ -9,34 +9,40 @@ path = os.path.join(os.path.expanduser("~"), "Pictures/PixivWallPaperPicCache")
 # time period to change the Picture (minutes)
 period = 1
 
+# detect if your platform is Windows or Linux
 system = platform.system()
-if not os.path.exists(path):
-    os.mkdir(path)
 picPath = os.path.join(path, "1.jpg")
 # anti-anti-crawler: User-Agent
 request_headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36"
 }
-url = "https://api.pixivic.com/illust?w_h_ratio=16-9&minWidth=1080&range=0.0001"
+url = "https://api.pixivic.com/illust?w_h_ratio=16-9&minWidth=1080&range=0.0001&isR18=false"
 
 while True:
-    if os.path.exists(picPath):
-        os.remove(picPath)
+    # detect if the cache folder exist, if not, make one
+    if not os.path.exists(path):
+        os.mkdir(path)
+    else:
+        # detect if there is cache picture, if so, delete it
+        if os.path.exists(picPath):
+            os.remove(picPath)
     req = request.Request(url, headers=request_headers)
-    print('getting URL of the Picture')
     try:
         realURL = request.urlopen(req).geturl()
     except:
         print('Error happened, trying again')
         continue
-    print('Downloading')
+    print('Got URL of the Picture')
     request.urlretrieve(realURL, picPath)
-    print('Setting as wallpaper')
+    print('Picture downloaded')
     if system == 'Windows':
-        ctypes.windll.user32.SystemParametersInfoW(20, 0, picPath, 0)  # set pic as wallpaper
+        # set pic as wallpaper
+        ctypes.windll.user32.SystemParametersInfoW(20, 0, picPath, 0)
     if system == 'Linux':
+        # set pic as wallpaper
         os.system('gsettings set org.gnome.desktop.background picture-uri "file:///%s"' % (picPath))
-    print('zzzzz')
+    print('Wallpaper updated')
+    print('waiting for %d more minutes' % (period))
     time.sleep(period*60)  # time period to change the Pic
 
 
